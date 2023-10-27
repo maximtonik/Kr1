55d002e9a90d99a6e4df18abacd2e25a9d9693e0
maximtonik <144106998+maximtonik@users.noreply.github.com>
2023-10-27 14:10:18 +0300

    Add files via upload

---
+++ ex1.pas
@@ -0,0 +1,16 @@
+﻿program ex1;
+
+var
+  x, y: real;
+
+begin
+  Writeln('Введите значение x ', x);
+  Readln(x);
+  if x < -8 then
+    Writeln('y = ', ln(x) - sqr(x):1:2) else
+  if (x < -2) then
+    Writeln('y = ', sqr(x) * sqr(x) * x + sin(x) / tan(x):1:2) else
+  if (x > -2) then
+    Writeln('y = ', 73 / sqr(x) - ln(x) / cos(x):1:2) else
+  
+end.
---
+++ ex2.pas
@@ -0,0 +1,18 @@
+﻿program ex2;
+
+var
+  x: real;
+
+begin
+  x := -10;
+  while x <= 0 do
+  begin
+    if x < -8 then
+      Writeln('y = ', ln(x) - sqr(x):1:2) else
+    if (x < -2) then
+      Writeln('y = ', sqr(x) * sqr(x) * x + sin(x) / tan(x):1:2) else
+    if (x < -2) then
+      Writeln('y = ', 73 / sqr(x) - ln(x) / cos(x):1:2);
+    x := x + 0.2;
+  end;
+end.
